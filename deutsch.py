e79656d61896a380177731aefd14474ffcd183d0
KelvinHsu <kelvin840722@gmail.com>
2016-02-14 21:24:42 +0800

    Function: Verbbedeutung completed, continuously prompting

--- deutsch.py
+++ deutsch.py
@@ -6,21 +6,23 @@ import re
 # Functions define
 def verbformen(key):
     # Source: http://www.verbformen.de/
-    response = urllib2.urlopen('http://www.verbformen.de/konjugation/?i=' + key)
+    url = 'http://www.verbformen.de/konjugation/?i=' + key
+    print(url)
+    response = urllib2.urlopen(url)
     html = response.read()
 
     # Navigate to the main article
     parse = BeautifulSoup(html, 'html.parser')
-    article = parse.find_all('article', class_='a')
-    div = article[0].find_all('div', class_='a')
-    sections = div[0].find_all('section')
+    article = parse.find('article', class_='a')
+    div = article.find('div', class_='a')
+    sections = div.find_all('section')
 
     # section: steckbrief-formen
-    steck = sections[0].find_all('div', id="steckbriefaufzu")
-    span = steck[0].find_all('span')
+    steck = sections[0].find('div', id="steckbriefaufzu")
+    span = steck.find_all('span')
     
     # Extract the wanted data
-    delete = re.compile('\xb7')
+    delete = re.compile('\xb7|er')
     tem_0 = span[0].getText()
     tem_1 = span[1].getText()
     tem_2 = span[2].getText()
@@ -38,9 +40,22 @@ def verbbedeutung(key):
     parse = BeautifulSoup(html, 'html.parser')
     mainfield = parse.find_all('div', class_="translation_lines")
     field = mainfield[0].find_all('div', class_='translation featured')
-    num = len(field)
     
+    definitions = []
+    for defs in field:
+        meanning = defs.find('a', class_='dictLink featured').getText()
+        definitions.append(meanning)
+    return definitions
     
 # Main func
-keyword = raw_input('Keyword:')
-print(verbformen(keyword))
+while True:
+    keyword = raw_input('Keyword (type \'exit\' to leave): ')
+    if keyword == 'exit':
+        break
+    else:
+        print( u"({} )".format(verbformen(keyword)) )
+        bedeutungen = verbbedeutung(keyword)
+        count = 1
+        for bedeutung in bedeutungen:
+            print("{}. {}".format(count, bedeutung))
+            count += 1
