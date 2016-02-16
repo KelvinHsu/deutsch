6f65f19784a5986afbae2436debe17a44e50bed3
KelvinHsu <kelvin840722@outlook.com>
2016-02-16 10:46:47 +0800

    New Feature: Force users to specify flags, which indicates what types are the keywords the users intend to search.

--- deutsch.py
+++ deutsch.py
@@ -1,17 +1,22 @@
 #!/Users/kelvin/Library/Enthought/Canopy_64bit/User/bin/pythonw
 # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
+import argparse
 import urllib
 import urllib2
 import re
 
-# Functions define
-def verbformen(key):
-    # Source: http://www.verbformen.de/
-    url = 'http://www.verbformen.de/konjugation/?i={}'.format(key)
-    print(url)
+##### Functions define #####
+def readurl(string, key):
+    url = string + key
+    #print url
     response = urllib2.urlopen(url)
     html = response.read()
+    return html
+
+def verbformen(key):
+    # Source: http://www.verbformen.de/
+    html = readurl('http://www.verbformen.de/konjugation/?i=', key)
 
     # Navigate to the main article
     parse = BeautifulSoup(html, 'html.parser')
@@ -34,13 +39,12 @@ def verbformen(key):
     return result.encode('utf-8')
 
 # Incomplete  
-def verbbedeutung(key):
+def bedeutung(key):
     # Source: http://www.linguee.com/english-german
-    url = 'http://www.linguee.com/english-german/search?source=auto&query=' + key
-    print url
-    response = urllib2.urlopen(url)
-    html = response.read()
+    linguee = 'http://www.linguee.com/english-german/search?source=auto&query='
+    html = readurl(linguee, key)
     
+    # Navigate to the definition area. Only show common definitions.
     parse = BeautifulSoup(html, 'html.parser')
     mainfield = parse.find_all('div', class_="translation_lines")
     field = mainfield[0].find_all('div', class_='translation featured')
@@ -51,17 +55,43 @@ def verbbedeutung(key):
         definitions.append(meanning)
     return definitions
     
-# Main func
+##### Main func #####
+# Parsing argument
+ap = argparse.ArgumentParser(description='Look up German verbs or nouns.')
+ap.add_argument('-v', action='store_const', dest='type',
+                const='verb',
+                help='Look up verbs')
+           
+ap.add_argument('-n', action='store_const', dest='type',
+                const='noun',
+                help='Look up nouns')
+                
+service = ap.parse_args()
+#print 'service = ', service.type
+
+# Looking up words
 while True:
+    if service.type is None:
+        print 'Please specify a flag (-v, or -n).'
+        break
+    
     #encoding = 'utf-8' if sys.stdin.encoding in (None, 'ascii') else sys.stdin.encoding
     keyword = raw_input('Keyword (type \'exit\' to leave): ')
     keyword = urllib.quote(keyword)
     if keyword == 'exit':
         break
+    elif keyword == '':
+        continue
     else:
-        print( "({} )".format(verbformen(keyword)) )
-        bedeutungen = verbbedeutung(keyword)
+        if service.type == 'verb':
+            print( "({} )".format(verbformen(keyword)) )
+        elif service.type == 'noun':
+            print 'comming soon'
+        
+        bedeutungen = bedeutung(keyword)
         count = 1
-        for bedeutung in bedeutungen:
-            print("{}. {}".format(count, bedeutung))
+        for term in bedeutungen:
+            print("{}. {}".format(count, term))
             count += 1
+            
+            
